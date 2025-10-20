31278a66c9566387e5113ba1399cc7b7f9b52a55
Marcelo Barros <marcelobarrossilvafilho@gmail.com>
2025-10-20 14:01:59 -0300

    feat: adiciona o mapeamento de rotas do ItensController

---
+++ src/main/java/com/marcelo/ControleDeEstoque/Itens/ItensController.java
@@ -0,0 +1,83 @@
+package com.marcelo.ControleDeEstoque.Itens;
+
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelo.ControleDeEstoque.Funcionarios.FuncionariosModel;
+import com.marcelo.ControleDeEstoque.Funcionarios.FuncionariosRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("itens")
+public class ItensController {
+    
+    private final ItensService itensService;
+    private final FuncionariosModel funcionarioAutenticado;
+
+    public ItensController(ItensService itensService, FuncionariosRepository funcionariosRepository){
+        this.itensService = itensService;
+        this.funcionarioAutenticado = funcionariosRepository.findById(UUID.fromString("8c9ba8b2-c04d-4b15-88a1-388a4613d3ec")).orElse(null);
+    }
+
+    @PostMapping("/POST")
+    public ResponseEntity<String> criarItem(@RequestBody ItensDTO itensDTO){
+        ItensDTO itemCriado = itensService.criarItens(itensDTO,funcionarioAutenticado);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Item " + itemCriado.getNome() + " criado com sucesso");
+    } 
+
+    @GetMapping("/GET")
+    public ResponseEntity<List<ItensDTO>> listarTodos() {
+        List<ItensDTO> listDTO = itensService.listarTodosItens();
+        return ResponseEntity.ok(listDTO);
+    }
+
+    @GetMapping("/GET/{id}")
+    public ResponseEntity<ItensDTO> listarPorId(@PathVariable UUID id) {
+        return ResponseEntity.ok(itensService.listarItensPorId(id));
+    }
+
+    @PutMapping("/PUT/{id}")
+    public ResponseEntity<ItensDTO> atualizarItem(@PathVariable UUID id, @RequestBody ItensDTO data) {
+        ItensDTO itemAtualizado = itensService.atualizarItem(id, data, funcionarioAutenticado);
+        
+        return ResponseEntity.ok(itemAtualizado);
+    }
+    
+    @PutMapping("/PUT/entrada/{id}")
+    public ResponseEntity<ItensDTO> entradaQtd(@PathVariable UUID id, @RequestBody QtdDTO qtdDTO) {
+        ItensDTO itemAtualizado = itensService.entradaQtd(id, qtdDTO.getQuantidade(), funcionarioAutenticado);
+        
+        return ResponseEntity.ok(itemAtualizado);
+    }
+    
+    @PutMapping("/PUT/saida/{id}")
+    public ResponseEntity<ItensDTO> saidaQtd(@PathVariable UUID id, @RequestBody QtdDTO qtdDTO) {
+        ItensDTO itemAtualizado = itensService.saidaQtd(id, qtdDTO.getQuantidade(), funcionarioAutenticado);
+        
+        return ResponseEntity.ok(itemAtualizado);
+    }
+
+    @DeleteMapping("/DELETE/{id}")
+    public ResponseEntity<?> deletarItem(@PathVariable UUID id){
+        itensService.deletarItem(id, funcionarioAutenticado);
+        return ResponseEntity.noContent().build();
+    }
+    
+    
+}
