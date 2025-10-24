5bd4536dc5a60abe222330ad504b0529ac4c9ee9
Marcelo Barros <marcelobarrossilvafilho@gmail.com>
2025-10-24 02:09:24 -0300

    feat: cria um controller para registros

---
+++ src/main/java/com/marcelo/ControleDeEstoque/Registros/RegistrosController.java
@@ -0,0 +1,35 @@
+package com.marcelo.ControleDeEstoque.Registros;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("registros")
+public class RegistrosController {
+
+    RegistrosService registrosService;
+
+    public RegistrosController(RegistrosService registrosService){
+        this.registrosService = registrosService;
+    }
+
+    @GetMapping("/GET")
+    public ResponseEntity<List<RegistrosDTO>> listarTodos() {
+        List<RegistrosDTO> listDTO = registrosService.listarTodos();
+        return ResponseEntity.ok(listDTO);
+    }
+
+    @GetMapping("/GET/{id}")
+    public ResponseEntity<RegistrosDTO> listarPorId(@PathVariable UUID id) {
+        return ResponseEntity.ok(registrosService.listarPorId(id));
+    }
+}
